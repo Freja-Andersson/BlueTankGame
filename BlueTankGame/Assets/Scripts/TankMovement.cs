9a6a54e73b9a15b20923e28199ab16ccf35d24a4
Freja Andersson <freja.andersson08@gmail.com>
2026-04-16 10:59:29 +0200

    Movement needs work on rotation

--- BlueTankGame/Assets/Scripts/TankMovement.cs
+++ BlueTankGame/Assets/Scripts/TankMovement.cs
@@ -4,8 +4,10 @@ using UnityEngine.InputSystem;
 public class TankMovement : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float rotateSensitivity = 1f;
+    [SerializeField] bool inRotation;
 
-    Vector3 movement;
+    Vector3 movementDirection;
 
     InputAction moveAction;
 
@@ -24,11 +26,33 @@ public class TankMovement : MonoBehaviour
         HandleMovement();
     }
 
-    void HandleMovement()
+    void HandleMovement() // rotate the tank to face the movement direction
     {
-        Vector2 moveDirection = moveAction.ReadValue<Vector2>();
-        Vector3 movement = new Vector3(moveDirection.x, 0f, moveDirection.y);
+        Vector2 moveInput = moveAction.ReadValue<Vector2>();
+        movementDirection = Vector3.right * moveInput.x + Vector3.forward * moveInput.y;
 
-        tankRigidbody.MovePosition(transform.position + movement * moveSpeed * Time.fixedDeltaTime);
+        if(movementDirection.sqrMagnitude > 0.01f) // only rotates if there is movement 
+        {
+            inRotation = true;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movementDirection), Time.fixedDeltaTime * rotateSensitivity);
+        }
+        else
+        {
+            inRotation = false;
+        }
+
+        //fix so the tank only rotates, and not moves until the right direction and than move forward
+
+        if(inRotation) // only rotate to change the direction
+        {
+            tankRigidbody.MovePosition(transform.position + movementDirection * moveSpeed * Time.fixedDeltaTime);
+        }
+        if (!inRotation) // move the tank forward in the direction
+        {
+            tankRigidbody.MovePosition(transform.position + movementDirection * moveSpeed * Time.fixedDeltaTime);
+        }
+        
     }
+
+    
 }
