75b4bb1da302256c5638bacac5316e6adf423b4c
MatildaSU24B <matildaljunggren08@gmail.com>
2026-04-30 10:44:05 +0200

    Fix
    Shoot fix

--- BlueTankGame/Assets/Matilda/Shoot.cs
+++ BlueTankGame/Assets/Matilda/Shoot.cs
@@ -1,4 +1,4 @@
-using UnityEditor.PackageManager;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,12 +6,15 @@ public class TankShoot : MonoBehaviour
 {
     [SerializeField]private InputAction ShootAction;
     private InputAction LookAction;
+    bool ShootDelay = false;
+    [SerializeField] float shootCooldown = 1f;
     [SerializeField] Vector3 lookDirection;
     [SerializeField] float Sensetivity = 5f;
     [SerializeField] float ShootRange = 20f;
     [SerializeField] Animator anim;
     [SerializeField] AttackFxPool fxPool;
     [SerializeField] GameObject Bullet;
+
 
 
     void Start()
@@ -24,20 +27,27 @@ public class TankShoot : MonoBehaviour
     {       
        Vector2 lookInput = LookAction.ReadValue<Vector2>();
        Vector3 lookDirection = new Vector3(lookInput.x, 0f, lookInput.y);
+
        if (lookDirection.sqrMagnitude > 0.01f)
        {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(lookDirection), Time.deltaTime * Sensetivity);
        }
 
-      
-       if (ShootAction.WasPerformedThisFrame())
+       if (ShootAction.WasPerformedThisFrame() && ShootDelay == false)
        {
             print("shoot");
             anim.SetTrigger("attack");
             fxPool.SpawnFX();
             Instantiate(Bullet, transform.position, transform.rotation);
+            StartCoroutine(ShootCooldown());
+        }
 
-           
-       }
+    }
+
+    IEnumerator ShootCooldown()
+    {
+        ShootDelay = true;
+        yield return new WaitForSeconds(shootCooldown);
+        ShootDelay = false;
     }
 }
