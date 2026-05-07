ba24682fa225d3c33199adf436c9bf3f4109a2fb
MatildaSU24B <matildaljunggren08@gmail.com>
2026-05-07 09:05:52 +0200

    Health

---
+++ BlueTankGame/Assets/Matilda/HealthTank.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class HealthTank : MonoBehaviour
+{
+    HealthScript healthScript;
+    void Start()
+    {
+        healthScript = GetComponent<HealthScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
