2d2e641d7e30ca9288298ce60e0d6731ec2a871e
Freja Andersson <freja.andersson08@gmail.com>
2026-05-10 17:15:07 +0200

    .

--- BlueTankGame/Assets/Scripts/AssignGamepad.cs
+++ BlueTankGame/Assets/Scripts/AssignGamepad.cs
@@ -7,17 +7,54 @@ public class AssignGamepad : MonoBehaviour
     [SerializeField] PlayerInput player1;
     [SerializeField] PlayerInput player2;
 
-
-    void Start()
+    void OnEnable()
     {
-        if (player1 != null) player1.user.UnpairDevices();
-        if (player2 != null) player2.user.UnpairDevices();
+        InputSystem.onDeviceChange += OnDeviceChange;
+        AssignDevices();
     }
 
-    void Update()
+    void OnDisable()
     {
-        var gamepads = Gamepad.all;
+        InputSystem.onDeviceChange -= OnDeviceChange;
+    }
 
+    private void OnDeviceChange(InputDevice device, InputDeviceChange change)
+    {
+        if (change == InputDeviceChange.Added || change == InputDeviceChange.Removed)
+        {
+            AssignDevices();
+        }
+    }
+    void AssignDevices()
+    {
+        if (player1 == null || player2 == null) { return; }
+        if (player1 != null) player1.user.UnpairDevices();
+        if (player2 != null) player2.user.UnpairDevices();
+
+        var gamepads = Gamepad.all;
+        var keyboard = Keyboard.current;
+
+
+        if (gamepads.Count == 0)
+        {
+            InputUser.PerformPairingWithDevice(keyboard, player1.user);
+            InputUser.PerformPairingWithDevice(keyboard, player2.user);
+            RenamePlayers();
+        }
+        else if (gamepads.Count == 1)
+        {
+            InputUser.PerformPairingWithDevice(gamepads[0], player1.user);
+            InputUser.PerformPairingWithDevice(keyboard, player2.user);
+            RenamePlayers();
+        }
+        else if (gamepads.Count == 2)
+        {
+            InputUser.PerformPairingWithDevice(gamepads[0], player1.user);
+            InputUser.PerformPairingWithDevice(gamepads[1], player2.user);
+            RenamePlayers();
+        }
+
+        /*
         if (gamepads.Count > 0 && player1 != null)
         {
             var d0 = gamepads[0];
@@ -25,10 +62,10 @@ public class AssignGamepad : MonoBehaviour
             player1.user.AssociateActionsWithUser(player1.actions);
             player1.gameObject.name = "Player1";
 
-            if(gamepads.Count == 1 && player2 != null) // connect player 2 to arrows
+            if (gamepads.Count == 1 && player2 != null) // connect player 2 to arrows
             {
                 Debug.Log("Only one gamepad found");
-                
+
             }
 
         }
@@ -39,13 +76,18 @@ public class AssignGamepad : MonoBehaviour
             InputUser.PerformPairingWithDevice(d1, player2.user);
             player2.user.AssociateActionsWithUser(player2.actions);
             player2.gameObject.name = "Player2";
-        }
+        } */
 
-        if (gamepads.Count <= 0)  // connect player 1 to wasd and player 2 to arrows
-        {
-            Debug.Log("no gamepads found");
-        }
 
+        player1.user.AssociateActionsWithUser(player1.actions);
+        player2.user.AssociateActionsWithUser(player2.actions);
+
+    }
+
+    void RenamePlayers()
+    {
+        player1.gameObject.name = "Player1";
+        player2.gameObject.name = "Player2";
     }
 
 }
